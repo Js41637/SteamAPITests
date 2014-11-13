491dac1d6436d95a6be4b533e3874b48d1da197f
Js41637 ftw <js41637@gmail.com>
2014-11-14 02:14:44 +1100

    Overhaul core code, create SteamAPI Interface and FriendsList directive
    * Removed old code for gathering SteamAPI data and created a new
    Factory/Provider which handles all Steam API requests
    * Changed friends list from using a div to it's own directive for future
    additions to the friends list overview.
    * Created friends.html as the friends list template

--- main.js
+++ main.js
@@ -1,12 +1,42 @@
 var app = angular.module('steamtrade', [])
 
-app.controller('MainCtrl', function($scope, $q, $http) {
+app.controller('MainCtrl', function($scope, steamAPI) {
 
   $scope.steamApiKey = "";
   $scope.pSteamID = "";
 
   $scope.returnError = ['false', null];
 
+  $scope.getProfile = function(){
+    steamAPI.getProfile($scope.steamApiKey, $scope.pSteamID).then(function(response) {
+      $scope.p_profile = response.data.response.players[0];
+      $scope.returnError = ['false', null];
+    }, function(err) {
+        $scope.returnError = ['true', err];
+    })
+  };
+
+  $scope.getFriends = function(){
+    steamAPI.getFriends($scope.steamApiKey, $scope.pSteamID).then(function(response) {
+      $scope.friendslist = response.data.friendslist.friends;
+      $scope.returnError = ['false', null];
+    }, function(err) {
+        $scope.returnError = ['true', err];
+    })
+  };
+
+  $scope.getServers = function(){
+    steamAPI.getServers($scope.steamApiKey).then(function(response) {
+      $scope.servers = response.data.response.servers;
+      $scope.returnError = ['false', null];
+    }, function(err) {
+        $scope.returnError = ['true', err];
+    })
+  };
+
+
+  
+/**
   $scope.getServers = function() {
     $http.get("https://api.steampowered.com/IGameServersService/GetAccountList/v0001/?key="+$scope.steamApiKey+"&format=json").then(
       function(response) {
@@ -19,10 +49,12 @@ app.controller('MainCtrl', function($scope, $q, $http) {
     );
   };
 
+
   $scope.getFriends = function() {
     $http.get("http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key="+$scope.steamApiKey+"&steamid="+$scope.pSteamID).then(
       function(response) {
         $scope.friends = response.data.friendslist.friends;
+        console.log(response.data.friendslist.friends)
         $scope.returnError = ['false', null];
       }, function(err) {
         $scope.returnError = ['true', err];
@@ -30,6 +62,7 @@ app.controller('MainCtrl', function($scope, $q, $http) {
       }
     );
   };
+  
 
   $scope.getProfile = function() {
     $http.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key="+$scope.steamApiKey+"&steamids="+$scope.pSteamID).then(
@@ -44,7 +77,7 @@ app.controller('MainCtrl', function($scope, $q, $http) {
       }
     );
   };
-
+*/
   $scope.getErrorMsg = function(err) {
     if(err == '403') {
       return "403 - Invalid API Key";
@@ -60,3 +93,48 @@ app.controller('MainCtrl', function($scope, $q, $http) {
     }
   }
 });
+
+app.directive('steamFriend', function(){
+  return {
+    restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+    templateUrl: 'friends.html',
+    transclude: true,
+    link: function($scope) {
+      
+    }
+  };
+});
+
+/** 
+ * Steam API Endpoint
+ */
+app.factory('steamAPI', function($q, $http){
+
+  var endpoints = {
+    servers: 'https://api.steampowered.com/IGameServersService/GetAccountList/v0001/?key=%key%',
+    friendslist: 'http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=%key%&steamid=%ids%',
+    playersummary: 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=%key%&steamids=%ids%'
+  };
+
+  /** 
+   * Get one of the urls from the endpoint and replace the parameters in it when provided.
+   */ 
+  var getUrl = function(type, apikey, ids) {
+    var out = endpoints[type].replace('%key%', apikey);
+    if(ids !== undefined) out = out.replace('%ids%', ids);
+    return out;
+  };
+
+  var service = {
+    getFriends: function(apikey, steamids) {
+      return $http.get(getUrl('friendslist', apikey, steamids));
+    },
+    getServers: function(apikey) {
+      return $http.get(getUrl('servers', apikey));
+    },
+    getProfile: function(apikey, steamids) {
+      return $http.get(getUrl('playersummary', apikey, steamids));
+    }
+  };
+  return service;
+})
