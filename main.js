c17fe5f1d7fb08c9d2e5d2aba6a2f4da3e6df71d
Js41637 ftw <js41637@gmail.com>
2014-11-14 02:38:10 +1100

    Added support for invalid steamIDS

--- main.js
+++ main.js
@@ -9,6 +9,11 @@ app.controller('MainCtrl', function($scope, steamAPI) {
 
   $scope.getProfile = function(){
     steamAPI.getProfile($scope.steamApiKey, $scope.pSteamID).then(function(response) {
+      if(response.data.response.players.length == 0) {
+        $scope.returnError = ['true', 'invalid'];
+        $scope.p_profile = null;
+        return;
+      }
       $scope.p_profile = response.data.response.players[0];
       $scope.returnError = ['false', null];
     }, function(err) {
@@ -34,59 +39,18 @@ app.controller('MainCtrl', function($scope, steamAPI) {
     })
   };
 
-
-  
-/**
-  $scope.getServers = function() {
-    $http.get("https://api.steampowered.com/IGameServersService/GetAccountList/v0001/?key="+$scope.steamApiKey+"&format=json").then(
-      function(response) {
-        $scope.servers = response.data.response.servers;
-        $scope.returnError = ['false', null];
-      }, function(err) {
-        $scope.returnError = ['true', err];
-        console.log($scope.returnError);
-      }
-    );
-  };
-
-
-  $scope.getFriends = function() {
-    $http.get("http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key="+$scope.steamApiKey+"&steamid="+$scope.pSteamID).then(
-      function(response) {
-        $scope.friends = response.data.friendslist.friends;
-        console.log(response.data.friendslist.friends)
-        $scope.returnError = ['false', null];
-      }, function(err) {
-        $scope.returnError = ['true', err];
-        console.log($scope.returnError);
-      }
-    );
-  };
-  
-
-  $scope.getProfile = function() {
-    $http.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key="+$scope.steamApiKey+"&steamids="+$scope.pSteamID).then(
-      function(response) {
-        //Returns array of SteamIDs but in this case, we're only requesting one
-        //so we need to select the first (and only) one with [0]
-        $scope.myProfile = response.data.response.players[0];
-        $scope.returnError = ['false', null];
-      }, function(err) {
-        $scope.returnError = ['true', err];
-        console.log($scope.returnError);
-      }
-    );
-  };
-*/
   $scope.getErrorMsg = function(err) {
-    if(err == '403') {
+    if(err.status == '403') {
       return "403 - Invalid API Key";
     }
-    if(err == '500') {
+    else if(err.status == '500') {
       return "500 - Invalid SteamID - Internal Server Error"
     }
-    if(err == '401') {
+    else if(err.status == '401') {
       return "401 - Private or Friends Only account - Unauthorized"
+    }
+    else if(err == 'invalid') {
+      return "SteamID does not exist or correspond to an account"
     }
     else {
       return "Something went wrong, error code: "+err;
